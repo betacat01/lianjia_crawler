53a4cc246a8984717098b7f27e4a68abec60327f
ray <3309606086@qq.com>
2018-04-22 22:11:22 +0800

    '链家网异步爬虫'

---
+++ lianjia_crawler.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+
+
+import time
+import asyncio
+import aiohttp
+from asyncio import Queue
+from collections import OrderedDict
+from lxml import etree
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+
+
+async def requests(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers={"User-Agent": ua.random}) as rep:
+            content = await rep.text()
+            return content
+
+
+def parse_root_page(url, content):
+    if url is None or content is None:
+        return
+
+    selector = etree.HTML(content)
+    nodes = selector.xpath("//ul[@id='house-lst']/li")
+    for node in nodes:
+        items = OrderedDict()
+        link = node.xpath(".//h2/a/@href")
+        name = node.xpath(".//h2/a/text()")
+        price = node.xpath(".//div[@class='price']/span/text()")
+        time = node.xpath(".//div[@class='other']/div/text()")
+        view_times = node.xpath(".//div[@class='square']/div/span/text()")
+        if link:
+            items["link"] = link[0]
+            # self.url_queue.put_nowait(link[0])
+        else:
+            items["link"] = ''
+        items["name"] = name[0] if name else ''
+        items["price"] = price[0] if price else ''
+        items["time"] = time[1] if len(time) == 2 else ''
+        items["view_times"] = view_times[0] if view_times else ''
+        # yield self.parse_detail_page(meta={"items": items})
+        print(items)
+
+
+async def main():
+    for i in range(500):
+        base_url = "https://bj.lianjia.com/zufang/pg{}/".format(i)
+        content = await requests(base_url)
+        parse_root_page(base_url, content)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    start = time.time()
+    loop.run_until_complete(main())
+    print("任务完成，耗时%d" % (time.time() - start))
